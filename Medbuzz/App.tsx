0447a6f63a4f654a706b7f30ee9d2ccff35a25b9
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2024-03-27 21:49:47 -0700

    Medbuzz Native Project
    Initial commit with Components and Screen folder. removed all default app.tsx containers that come preinstalled.

---
+++ Medbuzz/App.tsx
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View><Text>App.tsx</Text></View>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
