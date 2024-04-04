93ec6195486bccb642d4c9e2241704218b29e41b
Maddie <manandasubramanian@csus.edu>
2024-04-04 12:11:42 -0700

    License + License Location
    License page and license location page completed. Might have bugs, it works on my end though.

--- Medbuzz/App.tsx
+++ Medbuzz/App.tsx
@@ -1,43 +1,35 @@
-
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import Login from './Screens/Login.tsx'
+import Licenses from './Screens/Licenses.tsx';
+import LicensesLocation from './Screens/LicenseLocation';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button,
 } from 'react-native';
-
-// import { Not sure if there is needed 
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
 
 const Stack = createNativeStackNavigator();
 
 function App(){
 
-  return (
-    // in Stack.Navigator => initialRouteName="Login" screenOptions={{headerShown: false}}
-    <NavigationContainer>
-      <Stack.Navigator> 
-        <Stack.Screen name="Login" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Licenses" component={Licenses} />
+                <Stack.Screen name="LicensesLocation" component={LicensesLocation} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
- 
+
 });
 
 export default App;
--- Medbuzz/Screens/Login.tsx
+++ Medbuzz/Screens/Login.tsx
@@ -2,11 +2,11 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 const Login = () => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>This is Login</Text>
-    </View>
-  )
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>This is Login</Text>
+        </View>
+    )
 }
 
 export default Login
