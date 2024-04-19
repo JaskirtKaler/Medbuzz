c5664478c6035064c5168db9e831581b85fec897
Maddie <manandasubramanian@csus.edu>
2024-04-18 20:46:35 -0700

    MA-Sprint04 Commit
    Sprint 4 commit...

--- Medbuzz/Screens/Licenses.tsx
+++ Medbuzz/Screens/Licenses.tsx
@@ -1,12 +1,9 @@
-import React, {useState} from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import {Bar} from 'react-native-progress';
-import {Dropdown} from 'react-native-element-dropdown';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { Bar } from 'react-native-progress';
+import { Dropdown } from 'react-native-element-dropdown';
 import Backarrow from '../Components/Svg/Backarrow';
-import {useNavigation} from "@react-navigation/native";
-{
-    /* Dropdown */
-}
+import { useNavigation } from "@react-navigation/native";
 
 const Licenses = () => {
     const progress = 50; // progress percentage
@@ -14,14 +11,20 @@ const Licenses = () => {
     const navigation = useNavigation();
 
     const options = [
-        {label: 'Registered Nurse', value: 'Registered Nurse'},
-        {label: 'Licensed Practical Nurse', value: 'Licensed Practical Nurse'},
-        {label: 'Medical Doctor', value: 'Medical Doctor'},
-        {label: 'Doctor of Osteopathic Medicine ', value: 'Doctor of Osteopathic Medicine'},
-        {label: 'Pharmacist', value: 'Pharmacist'},
-        {label: 'Dentist ', value: 'Dentist '},
-        {label: 'Others ', value: 'Others '},
-        {label: '---', value: '---'},
+        { label: 'Registered Nurse (RN)', value: 'Registered Nurse (RN)' },
+        { label: 'Licensed Practical Nurse', value: 'Licensed Practical Nurse' },
+        { label: 'Medical Doctor (MD)', value: 'Medical Doctor (MD)' },
+        { label: 'Doctor of Osteopathic Medicine (DO)', value: 'Doctor of Osteopathic Medicine (DO)' },
+        { label: 'Nurse Practitioner (NP)', value: 'Nurse Practitioner (NP)' },
+        { label: 'Licensed Practical Nurse (LPN)', value: 'Licensed Practical Nurse (LPN)' },
+        { label: 'Physician Assistant (PA)', value: 'Physician Assistant (PA)' },
+        { label: 'Pharmacist', value: 'Pharmacist' },
+        { label: 'Dentist ', value: 'Dentist ' },
+        { label: 'Psychologist', value: 'Psychologist' },
+        { label: 'Physical Therapist', value: 'Physical Therapist' },
+        { label: 'Occupational Therapist', value: 'Occupational Therapist' },
+        { label: 'Other', value: 'Other' },
+        { label: '---', value: '---' },
     ];
 
     const handleBack = () => {
@@ -30,78 +33,45 @@ const Licenses = () => {
 
     const handleContinue = () => {
         console.log('Continue button clicked');
-        // navigation.navigate('LicensesLocation'); // Navigate to LicensesLocation screen
+        //navigation.navigate('LicensesLocation'); // Navigate to LicensesLocation screen
     };
 
     return (
-        <View
-            style={{
-                flex: 1,
-                backgroundColor: '#0EA68D',
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}>
-            {/* Back Button */}
-            <View style={{position: 'absolute', top: 10, left: 0}}>
+        <View style={styles.container}>
+            <View style={styles.backButtonContainer}>
                 <TouchableOpacity onPress={handleBack}>
                     <Backarrow width={40} height={40} color={'#FFF'} />
                 </TouchableOpacity>
             </View>
 
-            {/* Logo Placement */}
-            <View
-                style={{
-                    width: 100,
-                    height: 100,
-                    backgroundColor: 'white',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                <Text style={{color: '#0EA68D', fontSize: 65, fontWeight: 'bold'}}>
-                    M
-                </Text>
+            <View style={styles.logoContainer}>
+                <Text style={styles.logoText}>M</Text>
             </View>
 
-            {/* Progress Text */}
-            <Text
-                style={{
-                    color: 'black',
-                    marginTop: 30,
-                    marginLeft: 45,
-                    fontSize: 25,
-                    alignSelf: 'flex-start',
-                }}>
+            <Text style={styles.progressText}>
                 Progress: {progress}%
             </Text>
 
-            {/* Progress Bar */}
             <Bar
                 progress={progress / 100}
                 width={300}
                 color={'black'}
-                borderRadius={0} // remove the default amount of border radius that comes with the progress bar
-                unfilledColor={'#D9D9D9'} // Color of the unfilled portion of the progress bar, color gotten from figma
+                borderRadius={0}
+                unfilledColor={'#D9D9D9'}
                 height={20}
             />
 
-            <Text
-                style={{
-                    color: 'black',
-                    marginTop: 20,
-                    marginRight: 20,
-                    fontSize: 25,
-                }}>
+            <Text style={styles.headerText}>
                 Types of Licenses
             </Text>
 
-            {/* Dropdown */}
             <Dropdown
                 style={styles.dropdown}
                 selectedTextStyle={styles.selectedTextStyle}
                 inputSearchStyle={styles.inputSearchStyle}
                 data={options}
                 search
-                maxHeight={300}
+                maxHeight={150}
                 labelField="label"
                 valueField="value"
                 placeholder="Select Licenses"
@@ -109,17 +79,11 @@ const Licenses = () => {
                 value={selectedOption}
                 onChange={item => setSelectedOption(item.value)}
             />
-            {/* Continue Button */}
+
             <TouchableOpacity
                 onPress={handleContinue}
-                style={{
-                    backgroundColor: 'white',
-                    paddingHorizontal: 100,
-                    paddingVertical: 8,
-                    elevation: 5,
-                    marginBottom: 80,
-                }}>
-                <Text style={{color: '#0EA68D', fontSize: 25, fontWeight: 'bold'}}>
+                style={styles.continueButton}>
+                <Text style={styles.continueButtonText}>
                     Continue
                 </Text>
             </TouchableOpacity>
@@ -130,6 +94,43 @@ const Licenses = () => {
 export default Licenses;
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#0EA68D',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backButtonContainer: {
+        position: 'absolute',
+        top: 10,
+        left: 0,
+    },
+    logoContainer: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logoText: {
+        color: '#0EA68D',
+        fontSize: 65,
+        fontWeight: 'bold',
+    },
+    progressText: {
+        color: 'black',
+        marginTop: 30,
+        marginLeft: 10,
+        fontSize: 25,
+        alignItems: 'center',
+        marginBottom: 20,
+    },
+    headerText: {
+        color: 'black',
+        marginTop: 20,
+        marginRight: 20,
+        fontSize: 25,
+    },
     dropdown: {
         width: 300,
         margin: 16,
@@ -137,10 +138,6 @@ const styles = StyleSheet.create({
         borderBottomColor: 'black',
         borderBottomWidth: 1,
         marginBottom: 200,
-    },
-    placeholderStyle: {
-        fontSize: 16,
-        color: 'black',
     },
     selectedTextStyle: {
         fontSize: 16,
@@ -150,5 +147,17 @@ const styles = StyleSheet.create({
         height: 40,
         fontSize: 16,
         color: 'black',
+    },
+    continueButton: {
+        backgroundColor: 'white',
+        paddingHorizontal: 100,
+        paddingVertical: 8,
+        elevation: 5,
+        marginBottom: 80,
+    },
+    continueButtonText: {
+        color: '#0EA68D',
+        fontSize: 25,
+        fontWeight: 'bold',
     },
 });
