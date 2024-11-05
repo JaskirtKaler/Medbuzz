5e80db8b338cd5ffd6df495e0f5056fe3f60031e
JaskirtKaler <96710929+JaskirtKaler@users.noreply.github.com>
2024-11-04 22:05:42 -0800

    IOS config
    IOS config works and is able to redirect however there is a setting with deep linking that is causing errors in the Ceipal API as well as redirect is not opening the AD

--- Medbuzz/ios/Medbuzz/AppDelegate.h
+++ Medbuzz/ios/Medbuzz/AppDelegate.h
@@ -1,6 +1,10 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
 
-@interface AppDelegate : RCTAppDelegate
+
+@interface AppDelegate : RCTAppDelegate <UIApplicationDelegate, RNAppAuthAuthorizationFlowManager>
+
+@property(nonatomic, weak)id<RNAppAuthAuthorizationFlowManagerDelegate>authorizationFlowManagerDelegate;
 
 @end
