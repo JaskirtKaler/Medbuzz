94b41f2842c07da4d2105f1715e39df3dace8d98
Avneet Brar <avneetbrar@csus.edu>
2024-11-12 19:34:12 -0800

    Removed metro.config.js code for my environment

--- Medbuzz/metro.config.js
+++ Medbuzz/metro.config.js
@@ -6,10 +6,6 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {
-    resolver: {
-        useWatchman: false,
-    },
-};
+const config = {};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
